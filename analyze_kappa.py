387793b783a88f065a12c9522a6852935a823811
Ari <ari.schlakman@gmail.com>
2022-01-16 16:57:22 +0200

    Count offensive memes

--- analyze_kappa.py
+++ analyze_kappa.py
@@ -7,6 +7,9 @@ from FleissKappa.fleiss import fleissKappa
 def create_binary_scale(classification_matrix: List[List[int]]):
     binary_classification_matrix: List[List[int]] = list()
 
+    harmless_count = 0
+    offensive_count = 0
+
     for discrete_classification in classification_matrix:
         binary_classification = [0, 0]
         for i, value in enumerate(discrete_classification):
@@ -14,7 +17,15 @@ def create_binary_scale(classification_matrix: List[List[int]]):
                 binary_classification[0] += value
             else:
                 binary_classification[1] += value
+
+        if binary_classification[0] >= binary_classification[1]:
+            harmless_count += 1
+        else:
+            offensive_count += 1
         binary_classification_matrix.append(binary_classification)
+
+    print(f"Harmless: {harmless_count} Offensive: {offensive_count} "
+          f"Percent Offensive: {offensive_count / (offensive_count + harmless_count)}")
     return binary_classification_matrix
 
 
